0fdb8ec3fe70f3f89493c5267c6518b96a104a2e
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-08-02 02:15:06 +0300

    baseUrl

--- frontend/src/features/api/apiSlice.ts
+++ frontend/src/features/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:5002/api/',
+        baseUrl: import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api",
         prepareHeaders: (headers) => {
             const token = localStorage.getItem('token');
             if (token) {
