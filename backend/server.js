9f421bffc09962c6e615e5821fa1d2fdd44c0e91
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-08-02 04:00:09 +0300

    cors

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,10 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
-app.use(cors());
-
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 
 const server = http.createServer(app);
