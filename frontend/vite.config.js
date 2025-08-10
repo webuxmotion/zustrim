260ef7be15e5739c8fdebef76f9a763cb3b5f575
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-08-10 22:32:43 +0300

    Use /vite-plugin-node-polyfills

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,29 +1,29 @@
 import { defineConfig } from 'vite';
-import nodePolyfills from 'rollup-plugin-node-polyfills';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import tailwindcss from '@tailwindcss/vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    nodePolyfills(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
   optimizeDeps: {
-    include: ['events', 'util', 'buffer', 'process'],
+    include: ['events', 'util', 'buffer', 'process', 'stream'],
   },
   build: {
     sourcemap: true,
-    rollupOptions: {
-      plugins: [
-        nodePolyfills()
-      ]
-    },
+    // не треба додатково rollupOptions.plugins, nodePolyfills сам додасть поліфіли
   },
 });
